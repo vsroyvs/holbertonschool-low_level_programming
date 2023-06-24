6ecf97ed48834882cf9bc99c92eec8903ad17dda
Roy <vargas.sap321@gmail.com>
2023-06-24 10:34:40 -0700

    Program that finds and prints the sum of the even-valued terms of fibonacci serie whose values do not exceed 4m

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -9,20 +9,21 @@
 
 int main(void)
 {
-	unsigned long a, b, f, i; 
+	unsigned long a, b, f, i;
+
 	float sum = 0;
 
 	a = 0;
 	b = 1;
 	f = a + b;
 
-	for (i = 0; i < f; i++)
+	for (i = 0; i <= f; i++)
 	{
 		f = a + b;
 		if (f % 2 == 0)
 			sum = sum + f;
 
-		if (f + b > 20)
+		if (f + b > 4000000)
 			i = f + b;
 		a = b;
 		b = f;
