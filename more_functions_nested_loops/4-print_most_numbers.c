40237849484f1dc081407d73c34fc5e0965c46f2
Roy <vargas.sap321@gmail.com>
2023-06-25 08:07:01 -0700

    Function that prints the numbers from 0 -9 less 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Use _putchar function to print the alphabet 0 - 9 less 2 and 4
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		if (ch != '2' && ch != '4')
+			_putchar(ch);
+	}
+	_putchar('\n');
+}
