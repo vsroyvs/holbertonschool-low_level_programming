c1b3712f853579fd49913f5ac8ffe136ba666e42
Roy <vargas.sap321@gmail.com>
2023-06-25 11:17:59 -0700

    Fizz Buuzz test

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - point entry
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
