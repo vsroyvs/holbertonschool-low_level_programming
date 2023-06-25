0296e03551dae2387335121c0b0085379e03a907
Roy <vargas.sap321@gmail.com>
2023-06-25 07:55:57 -0700

    Function that prints the numbers from 0 to 9

---
+++ more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the caracter to print
+ *
+ * Return: Success 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
