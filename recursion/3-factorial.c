b14ae269b006d414bd2596cc200e3509699fd9c9
Roy <vargas.sap321@gmail.com>
2023-06-27 10:29:56 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-factorial.c -o 3-factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - function that returns the factorial of a given number
+ *
+ * @n: number will be operated
+ *
+ * Return: Always n! (Success)
+ */
+
+int factorial(int n)
+{
+	if (n == 1)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	return (n * factorial(n - 1));
+}
