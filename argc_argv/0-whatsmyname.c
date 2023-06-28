7e9edfc66350877c90817f0da853a1f34b4f7534
Roy <vargas.sap321@gmail.com>
2023-06-28 11:06:21 -0700

    program that prints its name, followed by a new line

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - point entry
+ * @argc: count arguments
+ * @argv: argument values
+ * Return: always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+		printf("%s\n", argv[0]);
+	return (0);
+}
