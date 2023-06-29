e531937c01c98044914a48577d3acd391633f2ef
Roy <vargas.sap321@gmail.com>
2023-06-29 08:13:13 -0700

    function that prints a string, followed by a new line.

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,11 +2,18 @@
 
 /**
  * _puts_recursion - function that prints a string
- *
+ * @s: pointer to char
  *
  */
 
 void _puts_recursion(char *s)
 {
-	
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+
 }
