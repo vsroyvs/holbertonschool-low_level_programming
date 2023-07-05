a034789c27311b510a41a53f6d559bceb3c15aed
Roy <vargas.sap321@gmail.com>
2023-07-05 14:27:55 -0700

    function that prints a name with pointer to functions

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Function that prints a name
+ *
+ * @name: pointer to char
+ * @f: pointer to function
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		(*f)(name);
+/**
+ * Also f(name), dont forget thar void (*f)(char *) is a pointer to function
+ *then (*f)(char *) = &print_name_uppercase
+ */
+}
